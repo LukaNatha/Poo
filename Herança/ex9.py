e74da489fa2f6986c8de72cb33c17b26f161e374
Luka <l.natha@escolar.ifrn.edu.br>
2026-04-16 02:11:08 +0000

    Ajuste para comprir objetivois do trabalho

--- Herança/ex9.py
+++ Herança/ex9.py
@@ -6,6 +6,9 @@ class Motor:
     
     def ligar_motor(self):
         return f"Motor de {self.potencia} cavalos"
+    
+    def exibir_info(self):
+        return f"Potência: {self.potencia} cavalos"
 
 @dataclass
 class Eletrico:
@@ -13,20 +16,31 @@ class Eletrico:
     
     def carregar(self):
         return f"Carregando bateria de {self.bateria}Ah" #fiqui em duvida da medida e pesquisei e coloquei amperes por hora
+    def exibir_info(self):
+        return f"Bateria: {self.bateria} Ah"
 
 @dataclass
 class Combustao:
     tanque: int
     
     def abastecer(self):
-        return f"Abastecendo tanque de {self.tanque} litros"
+        return f"Abastecendo tanque de {self.tanque} Litros"
+    
+    def exibir_info(self):
+        return f"Tanque: {self.tanque} Litros"
 
 @dataclass
 class CarroEletrico(Motor, Eletrico):
     def ligar(self):
         return f"{self.ligar_motor()} e {self.carregar()}"
+    
+    def exibir_info(self):
+        return f"{Motor.exibir_info(self)}, {Eletrico.exibir_info(self)}"
 
 @dataclass
 class CarroHibrido(Motor, Eletrico, Combustao):
     def ligar(self):
         return f"{self.ligar_motor()}, {self.carregar()} e {self.abastecer()}"
+    
+    def exibir_info(self):
+        return f"{Motor.exibir_info(self)}, {Eletrico.exibir_info(self)}, {Combustao.exibir_info(self)}"
