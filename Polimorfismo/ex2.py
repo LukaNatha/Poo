0583f30ac8a06259aa5b4cb9c429286613a8af5c
Luka <l.natha@escolar.ifrn.edu.br>
2026-04-23 01:50:13 +0000

    ex2

--- Polimorfismo/ex2.py
+++ Polimorfismo/ex2.py
@@ -1,18 +1,13 @@
-from dataclasses import dataclass
-
-@dataclass
 class Animal:
     def emitir_som(self):
         return "Algum som genérico"
 
-@dataclass
 class Cachorro(Animal):
     def emitir_som(self):
         som_original = super().emitir_som()
         print("Som original:", som_original)
         return "Cachorro:Au Au"
-
-@dataclass
+    
 class Gato(Animal):
     def emitir_som(self):
         som_original = super().emitir_som()
