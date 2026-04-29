d4d755b771dd47b7b9b30c583eb21afdb3a2f9a8
Luka <l.natha@escolar.ifrn.edu.br>
2026-04-29 16:43:16 +0000

    ex1 correction da correction

--- Coleções/ex1.py
+++ Coleções/ex1.py
@@ -1,9 +1,6 @@
 import random
 
 Loteria = random.sample(range(1, 41), 25)
-for numero in Loteria:
-    print(f"Número sorteado: {numero}")
-
 resultado = list(Loteria)
 resultado.sort()
 print(f"Lista de resultados: {resultado}")
