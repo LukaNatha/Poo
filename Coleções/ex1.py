4efd56c96bcbb5fc4e50459cd63544f634be1ea0
Luka <l.natha@escolar.ifrn.edu.br>
2026-05-01 20:21:15 +0000

    Atualização ex1.py

--- Coleções/ex1.py
+++ Coleções/ex1.py
@@ -1,6 +1,5 @@
 import random
 
 Loteria = random.sample(range(1, 41), 25)
-resultado = list(Loteria)
-resultado.sort()
-print(f"Lista de resultados: {resultado}")
+Loteria.sort()
+print(f"Lista de resultados: {Loteria}")
