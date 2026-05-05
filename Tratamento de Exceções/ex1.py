a87a551e5bde0412a15e6474f16dd73daf08f1e9
Luka <l.natha@escolar.ifrn.edu.br>
2026-05-05 18:33:59 +0000

    output change

--- Tratamento de Exceções/ex1.py
+++ Tratamento de Exceções/ex1.py
@@ -5,14 +5,14 @@ def adicionar_valor():
         raise Exception ("Somente valores positivos devem ser adicionados ao valor inicial")
     return inicial + adicional
 
-print("Teste com valor positivo")
+print("Teste com valor positivo\n")
 try:
     resultado = adicionar_valor()
     print(f"Resultado: {resultado}")
 except Exception as erro:
       print(f"Erro: {erro}")
     
-print("Teste com valor negativo")
+print("Teste com valor negativo\n")
 
 try:
      resultado = adicionar_valor()
